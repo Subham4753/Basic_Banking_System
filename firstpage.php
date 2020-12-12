604a292461f94e01413047c070d73bc936e1031c
Subham4753 <54077775+Subham4753@users.noreply.github.com>
2020-12-12 13:48:36 +0530

    Update firstpage.php

--- firstpage.php
+++ firstpage.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="firstpage.css">
 </head>
 <body>
-    <img src="bank8.jpg" alt="bank">
+    <img src="bank10.jpg" alt="bank">
     
     <div>
         <h1>Sparks Foundation Internship Project</h1>
